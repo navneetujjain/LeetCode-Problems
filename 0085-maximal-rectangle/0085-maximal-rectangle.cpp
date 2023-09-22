3c0f36362c8ae95e2e96403898de5864c5ede93b
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2023-09-22 18:20:52 +0530

    Time: 30 ms (88.6%), Space: 13.6 MB (70.79%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if(matrix.empty()) return 0;
+
+        int n = matrix[0].size();
+
+        vector<int> height(n + 1);
+
+        int maxArea = 0;
+
+        for(int i = 0; i < matrix.size(); i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == '1') height[j] = height[j] + 1;
+                else height[j] = 0;
+            }
+
+        stack<int> st;
+        st.push(-1);
+
+        for(int i = 0; i < n + 1; i++){
+            while(st.top() != -1 && height[st.top()] > height[i]){
+                int x = height[st.top()];
+                st.pop();
+                int width = i - st.top() - 1;
+                maxArea = max(maxArea, width * x);
+            }
+            st.push(i);
+        }
+    }
+
+        return maxArea;
+        
+    }
+};
