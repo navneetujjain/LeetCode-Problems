f9b9a5d5b67bd2dea97d7cf53bf5e9d77bff0b68
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2023-12-07 01:18:50 +0530

    Time: 47 ms (42.50%) | Memory: 20.7 MB (17.04%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+
+        int ans = 2;
+
+        if(n == 1) return 1;
+
+        for(int i = 0; i < n - 1; i++){
+            unordered_map<double, int> mpp;
+            for(int j = 0; j < n; j++){
+
+                if(j == i) continue;
+                int x1 = points[i][0];
+                int x2 = points[j][0];
+                int y1 = points[i][1];
+                int y2 = points[j][1];
+
+                double slope = atan2((y2 - y1),(x2 - x1));
+
+                mpp[slope]++;
+
+                ans = max(ans, mpp[slope] + 1);
+            }
+        }
+
+        return ans;
+    }
+};
