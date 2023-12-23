74883b3fcd78d9da3dfaca41cae05c104f59f559
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2023-12-23 17:00:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (17.13%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        set<pair<int, int>> stt;
+        stt.insert({0, 0});
+        int x = 0, y = 0;
+        for(char it : path){
+            if(it == 'N') y += 1;
+            else if(it == 'S') y -= 1;
+            else if(it == 'E') x += 1;
+            else x -= 1;
+
+            if(stt.find({x, y}) != stt.end()) return true;
+            stt.insert({x, y});
+        }
+
+        return false;
+    }
+};
