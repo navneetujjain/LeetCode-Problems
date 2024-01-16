eb092c35142bff551fe0a11b23418b06f23ba05b
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-01-16 22:11:55 +0530

    Time: 194 ms (32.17%) | Memory: 97.3 MB (73.46%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -36,11 +36,3 @@ public:
  * bool param_2 = obj->remove(val);
  * int param_3 = obj->getRandom();
  */
-
-/**
- * Your RandomizedSet object will be instantiated and called as such:
- * RandomizedSet* obj = new RandomizedSet();
- * bool param_1 = obj->insert(val);
- * bool param_2 = obj->remove(val);
- * int param_3 = obj->getRandom();
- */
