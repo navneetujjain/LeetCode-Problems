051820b37edc3419c43fdadaf0e7b16e3e3c3737
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-02-04 23:26:35 +0530

    Time: 29 ms (20.10%) | Memory: 11.2 MB (5.28%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string minWindow(string S, string T) {
+    string result;
+    if(S.empty() || T.empty()){
+        return result;
+    }
+    unordered_map<char, int> map;
+    unordered_map<char, int> window;
+    for(int i = 0; i < T.length(); i++){
+        map[T[i]]++;
+    }
+    int minLength = INT_MAX;
+    int letterCounter = 0;
+    for(int slow = 0, fast = 0; fast < S.length(); fast++){
+        char c = S[fast];
+        if(map.find(c) != map.end()){
+            window[c]++;
+            if(window[c] <= map[c]){
+                letterCounter++;
+            }
+        }
+        if(letterCounter >= T.length()){
+            while(map.find(S[slow]) == map.end() || window[S[slow]] > map[S[slow]]){
+                window[S[slow]]--;
+                slow++;
+            }
+            if(fast - slow + 1 < minLength){
+                minLength = fast - slow + 1;
+                result = S.substr(slow, minLength);
+            }
+        }
+    }
+    return result;
+}
+};
