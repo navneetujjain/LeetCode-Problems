3fa9cede74ff4526dfc970da902ed83efbb83e03
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-02-23 03:47:13 +0530

    Time: 58 ms (66.12%) | Memory: 51.3 MB (43.10%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let x = init;
+    return{
+        increment:() => {
+            return ++x;
+        },
+        decrement : () => {
+            return --x;
+        },
+        reset: () =>{
+            return x = init;
+        } 
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
