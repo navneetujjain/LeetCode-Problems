4b938f45a88e97e2191bbcc7d2bac086c468c308
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-03-13 13:45:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,12 +1,9 @@
+
+#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int pivotInteger(int n) {
-        double x = sqrt(n * (n + 1) / 2.0);
-        
-        if (fmod(x, 1.0) != 0) {
-            return -1;
-        } else {
-            return static_cast<int>(x);
-        }
+        double x=sqrt(n*(n+1)/2);
+        return x==(int)x?x:-1;
     }
 };
