08e3aa4fd5f14cd89a082e5a6bf17b24142a6d4d
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-03-13 13:45:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (41.86%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -1,9 +1,13 @@
-
-#pragma GCC optimize("O3", "unroll-loops")
 class Solution {
 public:
     int pivotInteger(int n) {
-        double x=sqrt(n*(n+1)/2);
-        return x==(int)x?x:-1;
+        int sum=n*(n+1)/2;      //sum from 1 to n;
+        for(int i=n/2;i<=n;i++)
+        {   
+            int sum_to_i=i*(i+1)/2;
+            if(sum_to_i==(sum-sum_to_i+i))return i; //there is +i because we want sum from i to n 
+                                                    //not from i-1 to n;
+        }
+        return -1;
     }
 };
