c87441aa7716329d23d103c7e4453a7fd2287873
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-03-13 13:45:25 +0530

    Time: 4 ms (35.26%) | Memory: 7.1 MB (73.82%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -5,8 +5,7 @@ public:
         for(int i=n/2;i<=n;i++)
         {   
             int sum_to_i=i*(i+1)/2;
-            if(sum_to_i==(sum-sum_to_i+i))return i; //there is +i because we want sum from i to n 
-                                                    //not from i-1 to n;
+            if(sum_to_i==(sum-sum_to_i+i))return i;
         }
         return -1;
     }
