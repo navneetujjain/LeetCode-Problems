e9d615063ec302bac08f7ba55871109134c8c9c2
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-03-20 23:51:44 +0530

    Time: 14 ms (60.15%) | Memory: 11.1 MB (64.56%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        long long int i = 0, j = 0, n = s.length(), count = 0, len = 0;
+        unordered_map<char, int> mpp;
+        for(; j < n; j++){
+            mpp[s[j]]++;
+
+            while(mpp[s[j]] > 1){
+                mpp[s[i++]]--;
+            }
+
+            count = max(count, j - i + 1);
+        }
+
+        return count;
+    }
+};
