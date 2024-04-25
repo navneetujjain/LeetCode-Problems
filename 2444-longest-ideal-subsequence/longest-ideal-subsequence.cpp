680e9f0a7e0a80df577f802819fd39bee7f2dd0b
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-04-25 18:57:14 +0530

    Time: 49 ms (72.27%) | Memory: 11.4 MB (84.45%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -3,8 +3,7 @@ public:
     int longestIdealString(string s, int k) {
         int dp[150] = {}, res = 0;
         for (auto i : s) {
-            for (int j = i - k; j <= i + k; ++j)
-                dp[i] = max(dp[i], dp[j]);
+            for (int j = i - k; j <= i + k; ++j) dp[i] = max(dp[i], dp[j]);
             res = max(res, ++dp[i]);
         }
         return res;
