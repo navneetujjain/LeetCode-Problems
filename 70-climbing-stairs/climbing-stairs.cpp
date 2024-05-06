a6daa11df4f9a574c0bced0dbf3da75a02c11257
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-05-06 15:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (65.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> dp;
-    int f(int n){
+    /*int f(int n){
         if(n < 0) return 0;
         if(n == 0) return 1;
 
         if(dp[n] != -1) return dp[n];
 
         return dp[n] = f(n - 1) + f(n - 2);
-    }
+    }*/
     int climbStairs(int n) {
         dp.resize(n + 1, -1);
+        dp[0] = 1;
 
-        return f(n);
+        for(int i = 1; i <= n; i++){
+            dp[i] = dp[i - 1];
+            if(i > 1) dp[i] += dp[i - 2];
+        }
+
+        return dp[n];
     }
 };
