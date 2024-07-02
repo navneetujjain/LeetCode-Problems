a0d9bd171f4f2c865d59d5a0b5d1417632032b2d
Navneet Ujjain <navneet.te20@bmsce.ac.in>
2024-07-03 02:14:46 +0530

    Time: 1333 ms (68.64%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,18 +1,13 @@
 SELECT
-            v.customer_id,
-            COUNT(v.visit_id)    count_no_trans
-
+        v.customer_id,
+        COUNT(v.visit_id) AS count_no_trans
 FROM
-            Visits v
-
+        Visits v
 LEFT JOIN
-            Transactions t
-
+        Transactions t
 ON
-            v.visit_id = t.visit_id
-
+        v.visit_id = t.visit_id
 WHERE
-            t.visit_id IS NULL
-
-GROUP BY
-            v.customer_id
+        t.visit_id IS NULL
+GROUP BY 
+        v.customer_id
